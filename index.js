3d589646b8469c18d1d6899ef369cd4bcb093c79
Max Ono <max@max-ono.com>
2021-01-23 23:05:43 -0800

    chore: Lint script

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const today = new Date().toDateString();
 
 // Groups array elements in an object by length
-const groupByLength = answers => (
+const groupByLength = (answers) =>
   answers.reduce((obj, answer) => {
     const length = answer.length;
 
@@ -10,104 +10,108 @@ const groupByLength = answers => (
     obj[length].push(answer);
 
     return obj;
-  }, {})
-);
+  }, {});
 
 // Fetches answe from NYTBee.com
-const fetchRemoteAnswers = async () => (
-  fetch('https://cors-anywhere.herokuapp.com/https://nytbee.com')
-    .then(response => response.text())
-    .then(html => {
+const fetchRemoteAnswers = async () =>
+  fetch("https://cors-anywhere.herokuapp.com/https://nytbee.com")
+    .then((response) => response.text())
+    .then((html) => {
       const domparser = new DOMParser();
-      const doc = domparser.parseFromString(html, 'text/html');
+      const doc = domparser.parseFromString(html, "text/html");
       const answers = [];
       const pangrams = [];
 
-      Array.from(doc.querySelectorAll("#main-answer-list ul li")).forEach(node => {
-        const word = node.innerText.trim();
+      Array.from(doc.querySelectorAll("#main-answer-list ul li")).forEach(
+        (node) => {
+          const word = node.innerText.trim();
 
-        answers.push(word);
+          answers.push(word);
 
-        if (node.querySelector('mark')) {
-          pangrams.push(word);
+          if (node.querySelector("mark")) {
+            pangrams.push(word);
+          }
         }
-      });
+      );
 
-      chrome.storage.local.set(({
-        'currentDate': today,
+      chrome.storage.local.set({
+        currentDate: today,
         answers,
-        pangrams
-      }));
+        pangrams,
+      });
 
       return { answers, pangrams };
     })
-    .catch(error => {
-      alert('Error fetching NYT Bee data');
+    .catch((error) => {
+      alert("Error fetching NYT Bee data");
       console.log(error);
-    })
-);
+    });
 
 // Fetch answers from browser storage
-const fetchLocalAnswers = async () => (
-  new Promise((resolve, _reject) => (
-    chrome.storage.local.get(['answers', 'pangrams'], items => resolve(items)))
-  ) 
-)
-    
-const determineCachedStatus = async () => (
-  new Promise((resolve, _reject) => (
-    chrome.storage.local.get('currentDate', ({ currentDate }) => resolve(today <= currentDate))
-  ))
-);
+const fetchLocalAnswers = async () =>
+  new Promise((resolve, _reject) =>
+    chrome.storage.local.get(["answers", "pangrams"], (items) => resolve(items))
+  );
 
-determineCachedStatus().then(isCached => {
-  const fetchAnswers = isCached ? fetchLocalAnswers : fetchRemoteAnswers
+const determineCachedStatus = async () =>
+  new Promise((resolve, _reject) =>
+    chrome.storage.local.get("currentDate", ({ currentDate }) =>
+      resolve(today <= currentDate)
+    )
+  );
+
+determineCachedStatus().then((isCached) => {
+  const fetchAnswers = isCached ? fetchLocalAnswers : fetchRemoteAnswers;
 
   fetchAnswers().then(({ answers, pangrams }) => {
     if (answers.length === 0) return;
 
     const grouped = groupByLength(answers);
-    const answerKey = document.createElement('ul');
-    answerKey.classList.add('sb-wordlist-items');
+    const answerKey = document.createElement("ul");
+    answerKey.classList.add("sb-wordlist-items");
 
     // Make list items for each word and insert into answer key
-    Object.values(grouped).forEach(words => (
-      words.forEach(word => {
-        const listItem = document.createElement('li');
-        listItem.style.webkitTextSecurity = 'disc';
+    Object.values(grouped).forEach((words) =>
+      words.forEach((word) => {
+        const listItem = document.createElement("li");
+        listItem.style.webkitTextSecurity = "disc";
 
-        const innerSpan = document.createElement('span');
-        innerSpan.classList.add('sb-anagram');
+        const innerSpan = document.createElement("span");
+        innerSpan.classList.add("sb-anagram");
         innerSpan.innerText = word;
 
         if (pangrams.includes(word)) {
-          listItem.style.color = '#f8cd05';
+          listItem.style.color = "#f8cd05";
         }
 
         listItem.appendChild(innerSpan);
         answerKey.appendChild(listItem);
       })
-    ));
+    );
 
-    const listContainer = document.querySelector('.sb-wordlist');
-    const gameList = listContainer.querySelector('.sb-wordlist-items');
-    const score = document.querySelector('.sb-wordlist-summary');
+    const listContainer = document.querySelector(".sb-wordlist");
+    const gameList = listContainer.querySelector(".sb-wordlist-items");
+    const score = document.querySelector(".sb-wordlist-summary");
 
     // Add the total word count to the score
-    const totalWords = document.createElement('span');
+    const totalWords = document.createElement("span");
     totalWords.innerText = ` (${answers.length} total)`;
     score.appendChild(totalWords);
 
     // Hide default UI list
-    gameList.style.display = 'none';
+    gameList.style.display = "none";
 
     // Display all currently guessed words in answer key
-    gameList.querySelectorAll('.sb-anagram').forEach(node => {
-      const answerKeyItems = Array.from(answerKey.querySelectorAll('.sb-anagram'));
-      const answerKeyItem = answerKeyItems.find(item => item.innerText.trim() === node.innerText.trim());
+    gameList.querySelectorAll(".sb-anagram").forEach((node) => {
+      const answerKeyItems = Array.from(
+        answerKey.querySelectorAll(".sb-anagram")
+      );
+      const answerKeyItem = answerKeyItems.find(
+        (item) => item.innerText.trim() === node.innerText.trim()
+      );
       if (!answerKeyItem) return;
 
-      answerKeyItem.style.webkitTextSecurity = 'none';
+      answerKeyItem.style.webkitTextSecurity = "none";
     });
 
     // Attach the answer key in place of the default UI list
@@ -115,20 +119,22 @@ determineCachedStatus().then(isCached => {
 
     // Instantiate an observer on the game list to see when a newly guessed word is added.
     // When one is, reveal the matching one in the answer key list.
-    const observer = new MutationObserver(mutation => {
-      const answerKeyItems = Array.from(answerKey.querySelectorAll('[style*="webkit-text-security: disc"]'));
-      const answerKeyItem = answerKeyItems.find(item => {
+    const observer = new MutationObserver((mutation) => {
+      const answerKeyItems = Array.from(
+        answerKey.querySelectorAll('[style*="webkit-text-security: disc"]')
+      );
+      const answerKeyItem = answerKeyItems.find((item) => {
         const match = item.innerHTML.match(/>(?<word>.*)</); // disc style makes innerText •s, so that no longer works here
         if (!match) return false;
-        
+
         return match[1].trim() === mutation[0].addedNodes[0].innerText.trim();
       });
       if (!answerKeyItem) return;
 
-      answerKeyItem.style.webkitTextSecurity = 'none';
+      answerKeyItem.style.webkitTextSecurity = "none";
     });
 
     // Start the observer
     observer.observe(gameList, { childList: true });
   });
-})
+});
