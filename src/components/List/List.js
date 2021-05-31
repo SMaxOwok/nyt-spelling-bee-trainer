b8c6b98b3626933e1ce528e381b2da8545cd61aa
Max Ono <max@max-ono.com>
2021-05-31 16:48:07 -0700

    feat: Implement list sorting

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,14 +1,24 @@
 import React from "react";
 import propTypes from "prop-types";
 
+import { groupByLength } from "helpers";
+
 import ListItem from "./ListItem";
 
 import "./List.css";
 
-export default function List({ answers, pangrams }) {
+export default function List({ answers, pangrams, sortBy }) {
+  const words = sortBy === "length" ? groupByLength(answers) : answers;
+
   return (
     <ul className="List">
-      {answers.map(answer => <ListItem key={answer} word={answer} isPangram={pangrams.includes(answer)} />)}
+      {words.map((answer) => (
+        <ListItem
+          key={answer}
+          word={answer}
+          isPangram={pangrams.includes(answer)}
+        />
+      ))}
     </ul>
   );
 }
@@ -16,4 +26,5 @@ export default function List({ answers, pangrams }) {
 List.propTypes = {
   answers: propTypes.array.isRequired,
   pangrams: propTypes.array.isRequired,
+  sortBy: propTypes.oneOf(["alpha", "length"])
 }
