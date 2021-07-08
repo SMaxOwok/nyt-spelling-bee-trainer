acd93a8279d274d7c89b89bb58a3241e24806510
Max Ono <max@max-ono.com>
2021-07-07 21:24:58 -0700

    feat: Add loading animation

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,6 +2,7 @@ import React from "react";
 import propTypes from "prop-types";
 import classNames from "classnames";
 
+import Bee from "assets/images/spelling-bee.svg";
 import { groupByLength } from "helpers";
 
 import ListItem from "./ListItem";
@@ -9,20 +10,28 @@ import ListItem from "./ListItem";
 import "./List.css";
 
 export default function List({ answers, pangrams, found, sortBy, isRevealed }) {
-  const listClasses = classNames("List", { "List--revealed": isRevealed });
+  const isLoading = !answers.length;
+  const listClasses = classNames("List", {
+    "List--revealed": isRevealed,
+    "List--loading": isLoading,
+  });
   const words = sortBy === "length" ? groupByLength(answers) : answers;
 
   return (
     <ul className={listClasses}>
-      {words.map((answer) => (
-        <ListItem
-          key={answer}
-          word={answer}
-          isPangram={pangrams.includes(answer)}
-          isFound={found.includes(answer)}
-          isRevealed={isRevealed}
-        />
-      ))}
+      {isLoading ? (
+        <img className="List__Bee" src={Bee} />
+      ) : (
+        words.map((answer) => (
+          <ListItem
+            key={answer}
+            word={answer}
+            isPangram={pangrams.includes(answer)}
+            isFound={found.includes(answer)}
+            isRevealed={isRevealed}
+          />
+        ))
+      )}
     </ul>
   );
 }
@@ -33,4 +42,4 @@ List.propTypes = {
   found: propTypes.array.isRequired,
   sortBy: propTypes.oneOf(["alpha", "length"]),
   isRevealed: propTypes.bool.isRequired,
-}
+};
